83f6f5221204f312f3391ddc5ee8c3a23b6502f3
Ahmed Alotaibi <aalot001@cslab-155.cs.plattsburgh.edu>
2017-11-12 15:34:47 -0500

    some changes to show readable information and show them

--- Directory.py
+++ Directory.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+
+
 # Ahmed Alotaibi, Akshay Singh
 # 11/8/2017
 # A program that store the files' data in a given directory
@@ -11,60 +13,70 @@ from os.path import isfile, join
 import pprint
 
 class Files():
-    """Returns all the files inside the directory without
-    looking into the subdirs in the form of alist"""
-    def filesInDir(self,directory):
-        return [listdir(directory)]
-    """Retrieves the files' properties and returns a dictionary containing the
-    file name as the key and another dictionary as its value with the details
-    of the file"""
-    def filesInDirWithInfo(self,directory):
-        values = {0:'protection bits: ',
-                  1:'inode number: ',
-                  2:'device: ',
-                  3:'number of hard links: ',
-                  4:'user id of owner: ',
-                  5:'group id of owner: ',
-                  6:'size of file in bytes: ',
-                  7:'time of most recent access: ',
-                  8:'time of most recent modification: ',
-                  9:'time of most metadata change: '}
-        files =  [[f,os.stat("{0}/{1}".format(directory,f))]
-                     for f in listdir(directory)]       
-        p = {files[filename][0]: {v:files[i][1][k]
-                for i in range(len(files))
-              for k,v in values.items() }
-            for filename in range(len(files)) }
-        return(p)
 
-    #Returns a list containing file names(and path) as well as the properties
-    def allFilesInfo(self,directory):
-        x = []
+    """Returns all files name in a given directory"""
+    def filesInDir(self, directory):
+        return [listdir(directory)]
+    """Directory: the directory to be searched for files
+       Returns: a dictionary with key of filename(and path)
+       and value of another dictionary containing the files'
+       properties.
+    """
+    def filesInDirWithInfo(self,directory):
+        values = {0:'protection bits: ',      1:'inode number: ',
+                  2:'device: ',               3:'number of hard links: ',
+                  4:'user id of owner: ',     5:'group id of owner: ',
+                  6:'size of file in bytes: ',7:'time of most recent access: ',
+                  8:'time of most recent modification: ',
+                              9:'time of most metadata change: '}
+        filepaths = []
         for dirpath, subdirs, files in os.walk(directory):
             for file in files:
-                x.append(os.path.join(dirpath,file))
-        data = [[x[p],os.stat(x[p])]
-                for p in range(len(x))]
-        return data
-    #Writes the returned value of allFilesInfo to an external file called
-    #"file data.txt"
+                filepaths.append(os.path.join(dirpath,file))
+        files =  [[f,os.stat(f)]
+                  for f in filepaths]
+        properties = {filepaths[i]:{v:files[i][1][k]
+                    for k,v in values.items() }
+                        for i in range(len(filepaths))}
+        return properties
+    """
+    Directory: the directory to be searched for files
+    using filesInDirWithInfo function.
+    Stores the files data as a string in a files called data.txt
+    """
     def store(self,directory):
+        k = self.filesInDirWithInfo(directory)
         with open('data.txt','w') as f:
-            k = [[j for j in i]
-                 for i in self.allFilesInfo(directory)]
-            f.write(str(k)+"\n")
-    #Checks to see if the data from the file is the same as the files' data now
+            f.write(str(k))
+            
+    """
+    Needs to be used after the store function
+    check for change in the current files using the filesInDirWithInfo
+    function against the text retrieved from the data.txt file
+    and report any changes to the files.
+    """
     def check(self,directory):
-        with open('file data.txt', 'r') as f:
-            ls = f.read()
-        k = [[j for j in i]
-             for i in self.allFilesInfo(directory)]
-        with open('file data.txt', 'r') as f:
-            old = f.read()
-        if old == str(k):
-            print('nothing changed')
+        new = self.filesInDirWithInfo(directory)
+        with open('data.txt', 'r') as f:
+            old = eval(f.read())
+        if new == old:
+            print('There is nothing changed')
         else:
-            print("something changed")
+            print("\nChanges found: \n")
+            for i in set(new).intersection(set(old)):
+                for j in new[i]:
+                    if new[i][j] != old[i][j]:
+                        print("\n\"{}\"\nFile path: /{}\
+                              \n\tNew value: {} to {}\n".format(j,i,old[i][j],new[i][j]))
+            for i in set(new).symmetric_difference(set(old)):
+                try:
+                    if old[i]:
+                        print("\nFile: /{}\
+                              has changed or is no longer exist".format(i))
+                except:
+                    if new[i]:
+                        print("\nChanges found: \n")
+                        print("The file /{}\ is a new file".format(i))
 
 def main():
     path = input("please enter the directory you want to search in: ")
@@ -76,8 +88,9 @@ def main():
     print("Files' information found in the given directory: ")
     pprint.pprint(test.filesInDirWithInfo(path))
     test.store(path)
-    print("you have 15 seconds to make changes")
-    time.sleep(15)
+    print("\n20 seconds to make changes the Directory such:\n\
+          Creating, editing, deleting file or whatever you like if exist")
+    time.sleep(20)
     test.check(path)
 
 if __name__== "__main__":
